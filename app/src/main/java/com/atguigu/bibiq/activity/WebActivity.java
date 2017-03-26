d893d565a10ede054731ab0f4f6b023b9e72a9dd
642609666 <642609666@qq.com>
2017-03-26 13:22:05 +0800

    分享以及浏览器打开页面完成

--- app/src/main/java/com/atguigu/bibiq/activity/WebActivity.java
+++ app/src/main/java/com/atguigu/bibiq/activity/WebActivity.java
@@ -1,38 +1,42 @@
 package com.atguigu.bibiq.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
-import android.widget.PopupWindow;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.atguigu.bibiq.R;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import butterknife.OnClick;
 
 public class WebActivity extends AppCompatActivity {
 
-    @InjectView(R.id.iv_web_back)
-    ImageView ivWebBack;
-    @InjectView(R.id.tv_web_name)
-    TextView tvWebName;
-    @InjectView(R.id.iv_web_more)
-    ImageView ivWebMore;
     @InjectView(R.id.webview)
     WebView webview;
-
-    private ProgressBar mProgressBar;
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    @InjectView(R.id.app_bar_layout)
+    AppBarLayout appBarLayout;
+    @InjectView(R.id.progressbar)
+    ProgressBar progressbar;
+    @InjectView(R.id.activity_web)
+    LinearLayout activityWeb;
+    String[] split;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +44,7 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
         ButterKnife.inject(this);
         String web_json = getIntent().getStringExtra("web_json");
-        String[] split = web_json.split(",,");
-        mProgressBar = new ProgressBar(this);
-        final AlertDialog dialog = new AlertDialog.Builder(this)
-                .setMessage("加载中......")
-                .setView(mProgressBar)
-                .show();
+         split = web_json.split(",,");
         WebSettings webSettings = webview.getSettings();
         //设置支持js
         webSettings.setJavaScriptEnabled(true);
@@ -63,12 +62,12 @@ public class WebActivity extends AppCompatActivity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                dialog.dismiss();
             }
 
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
+                progressbar.setVisibility(View.GONE);
                 return true;
             }
 
@@ -81,23 +80,73 @@ public class WebActivity extends AppCompatActivity {
             }
         });
         webview.loadUrl(split[0]);
-        tvWebName.setText(split[1]);
+        //设置头名字
+        toolbar.setTitle(TextUtils.isEmpty(split[1]) ? "详情" : split[1]);
+        setSupportActionBar(toolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
-    @OnClick({R.id.iv_web_back, R.id.iv_web_more})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.iv_web_back:
-                finish();
+    /**
+     * 记载menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_browser, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
                 break;
-            case R.id.iv_web_more:
-                PopupWindow popupWindow = new PopupWindow(ivWebMore,50,50);
 
-//                popupWindow.setBackgroundDrawable(new ColorDrawable(Color.parseColor("FF0000")));
+            case R.id.menu_share://分享
+                share();
+                break;
 
-                //（6）二者都有默认的背景，都可以通过setBackgroundDrawable(new ColorDrawable(android.R.color.transparent));去掉。
-                Toast.makeText(WebActivity.this, "分享", Toast.LENGTH_SHORT).show();
+            case R.id.menu_open: //浏览器打开
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(split[0]));
+                startActivity(intent);
+                break;
+
+            case R.id.menu_copy:
+               // ClipboardUtil.setText(WebActivity.this, split[0]);
+              //  Toast.makeText(WebActivity.this, "已复制", Toast.LENGTH_SHORT).show();
                 break;
         }
+        return super.onOptionsItemSelected(item);
+    }
+    //分享
+    private void share() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "分享");
+        intent.putExtra(Intent.EXTRA_TEXT, "来自「哔哩哔哩」的分享:" + split[0]); //传输网页
+        startActivity(Intent.createChooser(intent, split[1]));   //传出名字
+    }
+    @Override
+    public void onBackPressed() {
+
+        if (webview.canGoBack() && webview.copyBackForwardList().getSize() > 0
+                && !webview.getUrl().equals(webview.copyBackForwardList()
+                .getItemAtIndex(0).getOriginalUrl())) {
+            webview.goBack();
+        } else {
+            finish();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        webview.destroy();
+        super.onDestroy();
     }
 }
