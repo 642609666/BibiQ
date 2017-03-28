75bc0514f09c0498250856d9fbed933918f11708
642609666 <642609666@qq.com>
2017-03-29 00:26:12 +0800

    支付宝集成完事

---
+++ app/src/main/java/com/atguigu/bibiq/utils/zhifubao/PayResult.java
@@ -0,0 +1,87 @@
+package com.atguigu.bibiq.utils.zhifubao;
+
+import android.text.TextUtils;
+
+/**
+ * #                       _oo0oo_                     #
+ * #                      o8888888o                    #
+ * #                      88" . "88                    #
+ * #                      (| -_- |)                    #
+ * #                      0\  =  /0                    #
+ * #                    ___/`---'\___                  #
+ * #                  .' \\|     |# '.                 #
+ * #                 / \\|||  :  |||# \                #
+ * #                / _||||| -:- |||||- \              #
+ * #               |   | \\\  -  #/ |   |              #
+ * #               | \_|  ''\---/''  |_/ |             #
+ * #               \  .-\__  '-'  ___/-. /             #
+ * #             ___'. .'  /--.--\  `. .'___           #
+ * #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ * #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ * #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ * #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ * #                       `=---='                     #
+ * #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ * #                                                   #
+ * #               佛祖保佑         永无BUG             #
+ * #                                                     #
+ * #         QQ/微信: 642609666       李岩              #
+ */
+
+public class PayResult {
+    private String resultStatus;
+    private String result;
+    private String memo;
+
+    public PayResult(String rawResult) {
+
+        if (TextUtils.isEmpty(rawResult))
+            return;
+
+        String[] resultParams = rawResult.split(";");
+        for (String resultParam : resultParams) {
+            if (resultParam.startsWith("resultStatus")) {
+                resultStatus = gatValue(resultParam, "resultStatus");
+            }
+            if (resultParam.startsWith("result")) {
+                result = gatValue(resultParam, "result");
+            }
+            if (resultParam.startsWith("memo")) {
+                memo = gatValue(resultParam, "memo");
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "resultStatus={" + resultStatus + "};memo={" + memo
+                + "};result={" + result + "}";
+    }
+
+    private String gatValue(String content, String key) {
+        String prefix = key + "={";
+        return content.substring(content.indexOf(prefix) + prefix.length(),
+                content.lastIndexOf("}"));
+    }
+
+    /**
+     * @return the resultStatus
+     */
+    public String getResultStatus() {
+        return resultStatus;
+    }
+
+    /**
+     * @return the memo
+     */
+    public String getMemo() {
+        return memo;
+    }
+
+    /**
+     * @return the result
+     */
+    public String getResult() {
+        return result;
+    }
+}
---
+++ app/src/main/java/com/atguigu/bibiq/utils/zhifubao/SignUtils.java
@@ -0,0 +1,62 @@
+package com.atguigu.bibiq.utils.zhifubao;
+
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+/**
+ * #                       _oo0oo_                     #
+ * #                      o8888888o                    #
+ * #                      88" . "88                    #
+ * #                      (| -_- |)                    #
+ * #                      0\  =  /0                    #
+ * #                    ___/`---'\___                  #
+ * #                  .' \\|     |# '.                 #
+ * #                 / \\|||  :  |||# \                #
+ * #                / _||||| -:- |||||- \              #
+ * #               |   | \\\  -  #/ |   |              #
+ * #               | \_|  ''\---/''  |_/ |             #
+ * #               \  .-\__  '-'  ___/-. /             #
+ * #             ___'. .'  /--.--\  `. .'___           #
+ * #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ * #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ * #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ * #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ * #                       `=---='                     #
+ * #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ * #                                                   #
+ * #               佛祖保佑         永无BUG             #
+ * #                                                     #
+ * #         QQ/微信: 642609666       李岩              #
+ */
+
+public class SignUtils {
+    private static final String ALGORITHM = "RSA";
+
+    private static final String SIGN_ALGORITHMS = "SHA1WithRSA";
+
+    private static final String DEFAULT_CHARSET = "UTF-8";
+
+    public static String sign(String content, String privateKey) {
+        try {
+            PKCS8EncodedKeySpec priPKCS8 = new PKCS8EncodedKeySpec(
+                    Base64.decode(privateKey));
+            KeyFactory keyf = KeyFactory.getInstance(ALGORITHM);
+            PrivateKey priKey = keyf.generatePrivate(priPKCS8);
+
+            java.security.Signature signature = java.security.Signature
+                    .getInstance(SIGN_ALGORITHMS);
+
+            signature.initSign(priKey);
+            signature.update(content.getBytes(DEFAULT_CHARSET));
+
+            byte[] signed = signature.sign();
+
+            return Base64.encode(signed);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
